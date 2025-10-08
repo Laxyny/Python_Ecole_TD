928698fd67e13110466dabf500ef42453f8d7d98
Laxyny <kevgreg30@gmail.com>
2025-10-08 16:34:51 +0200

    *ADD* Changement de cap mais ça fonctionne

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 class Eleve:
     def __init__(self, nom, prenom, age, classe):
         self.nom = nom
@@ -10,15 +9,18 @@ class Eleve:
         return f"{self.prenom} {self.nom}, {self.age} ans, Classe : {self.classe}"
 
 
-class Ecole:
-    def __init__(self, nom_ecole, specialites, eleves=None):
+class Ecole(Eleve):
+    def __init__(
+        self, nom_ecole, specialites, eleves=None, nom="", prenom="", age=0, classe=""
+    ):
+        super().__init__(nom, prenom, age, classe)
         self.nom_ecole = nom_ecole
-        self.specialites = specialites 
+        self.specialites = specialites
         self.eleves = []
 
     def inscrire_eleve(self, eleve):
         self.eleves.append(eleve)
-        print(f" {eleve.prenom} {eleve.nom} a été inscri avec succès.")
+        print(f"{eleve.prenom} {eleve.nom} a été inscrit avec succès chez {self.nom_ecole}.")
 
     def afficher_eleves(self):
         if self.eleves is None:
@@ -28,4 +30,26 @@ class Ecole:
             for i, eleve in enumerate(self.eleves, 1):
                 print(f"{i}. {eleve}")
 
+ecole_keyce = Ecole(
+    nom_ecole="Keyce Informatique",
+    specialites=["Informatique"],
+)
 
+print(f"Nom: {ecole_keyce.nom_ecole}")
+print(f"Spécialités: {ecole_keyce.specialites} \n\n")
+
+eleve1 = Eleve(nom="GREGOIRE", prenom="Kevin", age=20, classe="Master 1")
+eleve2 = Eleve(nom="KODJO", prenom="Winner", age=20, classe="Master 1")
+eleve3 = Eleve(nom="PLEBANI", prenom="Lucas", age=20, classe="Master 1")
+
+#print(eleve1)
+#print(eleve2)
+#print(eleve3)
+
+ecole_keyce.afficher_eleves()
+
+ecole_keyce.inscrire_eleve(eleve1)
+ecole_keyce.inscrire_eleve(eleve2)
+ecole_keyce.inscrire_eleve(eleve3)
+
+ecole_keyce.afficher_eleves()
